da777fa59bf6925d6be0dcb2421aeb50a0457392
jzeng22 <jzeng22@jh.edu>
2023-01-08 10:17:40 -0600

    changed data analysis table

--- preliminary_data_analysis.py
+++ preliminary_data_analysis.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 from operator import add
 from operator import sub
 
-demographic = pd.read_csv(r'/Users/jennycinno/Desktop/math2neuro_project_3/demographic.csv')
-ERPdata = pd.read_csv(r'/Users/jennycinno/Desktop/math2neuro_project_3/ERPdata.csv')
+demographic = pd.read_csv(r'/Users/jennycinno/Documents/GitHub/math2neuro_project_3/demographic.csv')
+ERPdata = pd.read_csv(r'/Users/jennycinno/Documents/GitHub/math2neuro_project_3/ERPdata.csv')
 
 # creating a list of exact time stamps from 0 to 500 ms 
 num_time_stamps = 3072
@@ -96,6 +96,13 @@ for electrode in range(1, 10):
     ERP = average_ERP(1, electrode, 2)
     N100_patients_2.append(abs(min(ERP)))
     P200_patients_2.append(abs(max(ERP)))
+    
+# peak amplitude comparison between controls and patients 
+N100_CvsP_1 = list(map(sub, N100_controls_1, N100_patients_1))
+N100_CvsP_2 = list(map(sub, N100_controls_2, N100_patients_2))
+P200_CvsP_1 = list(map(sub, P200_controls_1, P200_patients_1))
+P200_CvsP_2 = list(map(sub, P200_controls_2, P200_patients_2))
+
 
 # changes in N100 and P200 between conditions 1 and 2 (negative #s indicate suppresion in condition 1, positive #s indicate increase in condition 1)
 N100_controls_1vs2 = list(map(sub, N100_controls_1, N100_controls_2))
@@ -107,9 +114,19 @@ P200_patients_1vs2 = list(map(sub, P200_patients_1, P200_patients_2))
 N100_CvsP = list(map(sub, N100_patients_1vs2, N100_controls_1vs2)) # positive #s mean controls suppressed more in condition 1
 P200_CvsP = list(map(sub, P200_controls_1vs2, P200_patients_1vs2)) # positive #s mean controls increased more in condition 1
 
+"""
 # creating a dataframe of the preliminary data analysis
 row_labels = ['Fz', 'FCz', 'Cz', 'FC3', 'FC4', 'C3', 'C4', 'CP3', 'CP4']
 column_labels = ['N100 C1', 'P200 C1', 'N100 C2', 'P200 C2', 'N100 P1', 'P200 P1', 'N100 P2', 'P200 P2', 'N100 C1vs2', 'P200 C1vs2', 'N100 P1vs2', 'P200 P1vs2', 'N100 Supression CvsP', 'P100 Increase CvsP']
 data = np.column_stack((N100_controls_1, P200_controls_1, N100_controls_2, P200_controls_2, N100_patients_1, P200_patients_1, N100_patients_2, P200_patients_2, N100_controls_1vs2, P200_controls_1vs2, N100_patients_1vs2, P200_patients_1vs2, N100_CvsP, P200_CvsP))
 data_analysis_df = pd.DataFrame(data, index = row_labels, columns = column_labels)
-data_analysis_df.to_csv('/Users/jennycinno/Desktop/math2neuro_project_3/data_analysis.csv')
+data_analysis_df.to_csv('/Users/jennycinno/Documents/GitHub/math2neuro_project_3/data_analysis.csv')
+"""
+
+# creating a dataframe of the preliminary data analysis
+row_labels = ['Fz', 'FCz', 'Cz', 'FC3', 'FC4', 'C3', 'C4', 'CP3', 'CP4']
+column_labels = ['N100 C1 vs P1', 'N100 C2 vs P2', 'P200 C1 vs P1', 'P200 C2 vs P2','N100 Supression C vs P', 'P100 Increase C vs P']
+data = np.column_stack((N100_CvsP_1, N100_CvsP_2, P200_CvsP_1, P200_CvsP_2, N100_CvsP, P200_CvsP))
+data_analysis_df = pd.DataFrame(data, index = row_labels, columns = column_labels)
+data_analysis_df.to_csv('/Users/jennycinno/Documents/GitHub/math2neuro_project_3/data_analysis.csv')
+
