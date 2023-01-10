533953f9972849aaf350a5e30251567619d8e819
jzeng22 <jzeng22@jh.edu>
2023-01-09 21:25:42 -0600

    added xgboost model

--- logistic_regression.py
+++ logistic_regression.py
@@ -5,6 +5,8 @@ from operator import add
 from operator import sub
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from xgboost import XGBClassifier
 
 demographic = pd.read_csv(r'/Users/jennycinno/Documents/GitHub/math2neuro_project_3/demographic.csv')
 ERPdata = pd.read_csv(r'/Users/jennycinno/Documents/GitHub/math2neuro_project_3/ERPdata.csv')
@@ -34,6 +36,18 @@ def patient_ERP(patient, electrode, condition):
 
 # feature extraction
 """
+N100_1_C = []
+N100_1_P = []
+for i in range(0, 81):
+    if demographic.iloc[i, 1] == 0:
+        N100_1_C.append(min(patient_ERP(i, 2, 2)))
+    elif demographic.iloc[i, 1] == 1:
+        N100_1_P.append(min(patient_ERP(i, 2, 2)))
+
+plt.boxplot([N100_1_C, N100_1_P])
+"""
+    
+"""
 # N100 peak amplitude in condition 1, electrode 1
 N100_1 = []
 for i in range(0, 81):
@@ -42,7 +56,7 @@ for i in range(0, 81):
 # N100 peak amplitude in condition 2, electrode 1
 N100_2 = []
 for i in range(0, 81):
-    N100_1.append(min(patient_ERP(i, 1, 2)))
+    N100_2.append(min(patient_ERP(i, 1, 2)))
 
 # P200 peak amplitude in condition 1, electrode 1
 P200_1 = []
@@ -59,6 +73,17 @@ N100_suppression = []
 for i in range(0, 81):
     N100_suppression.append(abs(min(patient_ERP(i, 1, 2))) - abs(min(patient_ERP(i, 1, 1))))
 """
+"""
+N100_suppression_C = []
+N100_suppression_P = []
+for patient in range(0, 81):
+    if demographic.iloc[patient, 1] == 0:
+        N100_suppression_C.append(abs(min(patient_ERP(patient, 9, 2))) - abs(min(patient_ERP(patient, 9, 1))))
+    elif demographic.iloc[patient, 1] == 1:
+        N100_suppression_P.append(abs(min(patient_ERP(patient, 9, 2))) - abs(min(patient_ERP(patient, 9, 1))))
+
+plt.boxplot([N100_suppression_C, N100_suppression_P])
+"""
 x = []
 for patient in range(0, 81):
     patient_x = []
@@ -74,12 +99,19 @@ y = []
 for patient in range(0, 81):
     y.append(demographic.iloc[patient, 1])
     
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
-     
-# logistic regression model    
-model = LogisticRegression(max_iter = 1000)
-model.fit(x_train, y_train)
-print(model.score(x_test, y_test))
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3)
+   
+# logistic regression model
+lr_model = LogisticRegression(max_iter = 1000)
+lr_model.fit(x_train, y_train)
+print(lr_model.score(x_test, y_test))
+
+# xgboost model
+xgb_model = XGBClassifier()
+xgb_model.fit(x_train, y_train)
+predictions = xgb_model.predict(x_test)
+print(accuracy_score(y_test, predictions))
+
 
 
 
